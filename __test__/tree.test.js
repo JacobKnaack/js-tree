4209cd0a8a9ef9446da896dcdc61ecfd957fe300
JacobKnaack <jacobknck@gmail.com>
2019-09-04 21:54:15 -0700

    adds leaf test

--- __test__/tree.test.js
+++ __test__/tree.test.js
@@ -8,7 +8,7 @@ describe('Testing binary tree classes', () => {
   const tree = new Tree();
   describe('Should have correct properties', () => {
     it('tree should have root', () => {
-      const match = {root:null};
+      const match = { root: null };
       expect(tree).toMatchObject(match);
     });
   });
@@ -16,15 +16,17 @@ describe('Testing binary tree classes', () => {
 
   describe('tree methods', () => {
     it('should be able to append nodes', () => {
-      expect(tree.append(20)).toMatchObject({value: 20, right: null, left: null});
-      expect(tree.append(30)).toMatchObject({value: 30, right: null, left: null});
-      expect(tree.root.left).toMatchObject({value: 30, right: null, left: null});
+      expect(tree.append(20)).toMatchObject({ value: 20, right: null, left: null });
+      expect(tree.append(30)).toMatchObject({ value: 30, right: null, left: null });
+      expect(tree.root.left).toMatchObject({ value: 30, right: null, left: null });
     });
     it('should be able to return all the leaves', () => {
-      expect(tree.leaves()).toMatchObject([new Node(30)]);
+      tree.append(40);
+      tree.append(50);
+      expect(tree.leaves()).toMatchObject([new Node(40), new Node(50)]);
     });
     it('Can traverse the tree', () => {
-      expect(tree.traverse({order: 'preorder', callback: (current) => current.value += 1}).value).toBe(21);
+      expect(tree.traverse({ order: 'preorder', callback: (current) => current.value += 1 }).value).toBe(21);
     });
   })
 });
